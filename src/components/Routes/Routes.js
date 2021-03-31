3bce0c251032819950f1546871366fae77a788fd
Tejas <tejasshah>
2021-03-31 12:06:08 +0530

    Correct route added for invoice manage

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,26 +1,26 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-import NotFound from "../NotFound";
-import StaffManagement from "../staffManagement/StaffManagement";
-import StaffAdminDashboard from "../staffManagement/StaffAdminDashboard";
+import { Route, Switch } from "react-router-dom";
+import About from "../About/About";
 import BigAbout from "../blogs/BigAbout";
 import Feature from "../blogs/Feature";
 import Score from "../blogs/Score";
-import Inventory from "../inventoryManagement/Invetory";
-import Invoice from "../InvoiceGeneration/Invoice";
-import Page from "../Profile_Page/Page";
-import RegistrationPage from "../Registration/Registration_Page";
-import MainFinance from "../Finance/MainFinance";
-import LoginMain from "../login/LoginMain";
-import ManageInvoiceTable from "../InvoiceManagement/ManageInvoiceTable";
-import PrivateRoute from "./PrivateRoute";
-import Dashboard from "../Home/Dashboard";
-import RegisterMain from "../login/RegisterMain";
-import Home from "../Home/Home";
-import About from "../About/About";
 import PaymentDues from "../Finance/Dues/PaymentDues";
+import MainFinance from "../Finance/MainFinance";
 import PaymentReminders from "../Finance/Reminders/PaymentReminders";
 import { TransactionsTable } from "../Finance/TransactionTable/TransactionsTable";
+import Dashboard from "../Home/Dashboard";
+import Home from "../Home/Home";
+import Inventory from "../inventoryManagement/Invetory";
+import Invoice from "../InvoiceGeneration/Invoice";
+import ManageInvoice from "../InvoiceManagement/ManageInvoice";
+import LoginMain from "../login/LoginMain";
+import RegisterMain from "../login/RegisterMain";
+import NotFound from "../NotFound";
+import Page from "../Profile_Page/Page";
+import RegistrationPage from "../Registration/Registration_Page";
+import StaffAdminDashboard from "../staffManagement/StaffAdminDashboard";
+import StaffManagement from "../staffManagement/StaffManagement";
+import PrivateRoute from "./PrivateRoute";
 // this function routes through different components by checking urlpath.
 function Routes() {
   return (
@@ -39,10 +39,10 @@ function Routes() {
             <Route exact path="/about" component={About} />
           <Route exact path="*" component={Home} /> */}
 
-          <Route exact path="/transaction" component={TransactionsTable} />
-          <Route exact path="/remainders" component={PaymentReminders} />
-          <Route exact path="/dues" component={PaymentDues} />
-          <Route exact path="/register" component={RegisterMain} />
+      <Route exact path="/transaction" component={TransactionsTable} />
+      <Route exact path="/remainders" component={PaymentReminders} />
+      <Route exact path="/dues" component={PaymentDues} />
+      <Route exact path="/register" component={RegisterMain} />
 
       {/* public Routes */}
       <Route exact path="/about" component={About} />
@@ -58,19 +58,15 @@ function Routes() {
       {/* <Route exact path="/MainFinance" component={MainFinance} /> */}
       <Route exact path="/login" component={LoginMain} />
       <Route exact path="/dashboard" component={Dashboard} />
-      <Route path="*" component={NotFound} />
       {/* <Route exact path="/page" component={Page} /> */}
       <PrivateRoute exact path="/mainfinance" component={MainFinance} />
       <PrivateRoute exact path="/staff" component={StaffManagement} />
       <PrivateRoute exact path="/inventory" component={Inventory} />
       <PrivateRoute exact path="/invoice" component={Invoice} />
+      <PrivateRoute exact path="/invoicemanage" component={ManageInvoice} />
       <PrivateRoute exact path="/Page" component={Page} />
-      <PrivateRoute
-        exact
-        path="/invoicemanage"
-        component={ManageInvoiceTable}
-      />
       <PrivateRoute exact path="/admin" component={StaffAdminDashboard} />
+      <Route path="*" component={NotFound} />
     </Switch>
   );
 }
